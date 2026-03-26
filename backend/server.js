f2d3fc22e2da17ae7160259812c02ea1c6e5692e
Theron Papillion <tpapillionjr@gmail.com>
2026-03-25 20:35:08 -0500

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
-const app = require('./src/app');
+//main server code
+import express from "express"
+import cors from "cors"
+import items from './src/routes/items.routes.js'
 
-const PORT = process.env.PORT || 4000;
+const app = express()
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/items",items)
+app.use((req, res) => {
+  res.status(404).json({ error: "not found" });
 });
+
+export default app
+
+
+//add a get api for the library for all the data. 
+//post for for server-order.js to checkout/index.js
+ 
