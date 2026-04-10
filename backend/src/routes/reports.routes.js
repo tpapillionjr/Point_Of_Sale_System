e39e1f3cebe02602149461c6b24025f41e81513b
MADuran713 <77908341+MADuran713@users.noreply.github.com>
2026-04-09 23:59:02 -0500

    formating for the backend

--- backend/src/routes/reports.routes.js
+++ backend/src/routes/reports.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { requireAuth, requireManager } from "../../middleware/auth.middleware.js";
+import { validateRange } from "../../middleware/validate-range.middleware.js";
 import { getDashboard, getOverview } from "../controllers/reports.controller.js";
 
 const router = express.Router();
+
+
+router.use(validateRange);
 
 router.get("/dashboard", requireAuth, requireManager, getDashboard);
 router.get("/overview", requireAuth, requireManager, getOverview);
