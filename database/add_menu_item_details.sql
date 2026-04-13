45196efcddb9e98437eb72b6dde3cd96d2b91280
Andy Do <145503608+aldo3100@users.noreply.github.com>
2026-04-13 01:10:56 -0500

    add morgan tirgger to schema and updated DB, removed add_menu_item_details.sql & add_online_confirm_kitchen_trigger.sql

--- database/add_menu_item_details.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE Menu_Item
-    ADD COLUMN description TEXT NULL AFTER base_price,
-    ADD COLUMN photo_url VARCHAR(2048) NULL AFTER description,
-    ADD COLUMN common_allergens VARCHAR(255) NULL AFTER photo_url;
